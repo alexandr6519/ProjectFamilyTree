94a41057d6f0a5b43944c32d555407eeddff4bce
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2023-04-11 13:34:50 +0700

    Update HumanComparatorByBirthday.java
    correcting of cod

--- FamTree/src/HumanComparatorByBirthday.java
+++ FamTree/src/HumanComparatorByBirthday.java
@@ -1,6 +1,6 @@
 import java.util.Comparator;
 
-public class HumanComparatorByBirthday implements Comparator<Human> {
+public class HumanComparatorByBirthday<H> implements Comparator<Human> {
     @Override
     public int compare(Human human_1, Human human_2) {
         return human_1.getBirthYear() - human_2.getBirthYear();
