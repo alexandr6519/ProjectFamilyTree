7615d89f41cfdb1dd0926fbf46b18a52e6adc01c
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2023-04-11 13:36:20 +0700

    Update HumanComparatorByName.java
    correcting of cod

--- FamTree/src/HumanComparatorByName.java
+++ FamTree/src/HumanComparatorByName.java
@@ -1,6 +1,6 @@
 import java.util.Comparator;
 
-public class HumanComparatorByName implements Comparator<Human> {
+public class HumanComparatorByName<H> implements Comparator<Human> {
     @Override
     public int compare(Human human_1, Human human_2) {
         return human_1.getFullName().compareTo(human_2.getFullName());
